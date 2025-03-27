2cee2ba7c8aafe68ac1b9a6ec11e3c317056abf7
JA Creative <dis1432346+1@gmail.com>
2025-03-27 12:23:20 -0400

    adjusted config file

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "https://jacreative904.github.io/JA-Creative/",
+  base: "/JA-Creative",
 })
