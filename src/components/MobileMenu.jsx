7514e71ba700634e765d3256f625b2ceadfbb793
JA Creative <dis1432346+1@gmail.com>
2025-04-10 18:42:18 -0400

    Styling for mobile menu, styling for loading screen

--- src/components/MobileMenu.jsx
+++ src/components/MobileMenu.jsx
@@ -6,7 +6,7 @@ import { Link } from 'react-router-dom';
 const MobileMenu = ({menuOpen, setMenuOpen}) => {
 
     return (
-        <div className={`fixed top-0 left-0 w-dvw bg-darkdark/95 z-40 flex flex-col  items-center justify-center transition-all duration-300 ease-in-out
+        <div className={`fixed uppercase top-0 left-0 w-dvw bg-mainteal/95 dark:bg-maindark/95 z-40 flex flex-col items-center text-center justify-center transition-all duration-300 ease-in-out
                             ${
                             menuOpen 
                                 ? "h-screen opacity-100 pointer-events-auto" 
@@ -34,6 +34,7 @@ const MobileMenu = ({menuOpen, setMenuOpen}) => {
                         }
                 `}>Home
             </Link>
+            <hr className='w-90 text-maingrey dark:text-darkyellow mt-[20px] mb-[20px]'/>
         </li>
         
         <li>
@@ -47,6 +48,7 @@ const MobileMenu = ({menuOpen, setMenuOpen}) => {
                                 : "opacity-0 translate-y-5"
                             }
                 `}>About
+            <hr className='w-90 text-maingrey dark:text-darkyellow mt-[20px] mb-[20px]'/>
             </Link>
         </li>
         
@@ -64,11 +66,11 @@ const MobileMenu = ({menuOpen, setMenuOpen}) => {
             </Link>
         </li>
         
-        <li>
+        <li className='mt-[40px]'>
             <Link 
             to="/contact" 
             onClick={() => setMenuOpen(false)}
-            className={`flex font-bold text-center nav-cta space-x-3 my-4 transform transition-transform duration-300
+            className={`text-4xl text-darklight dark:text-darkbg transform transition-transform duration-300 bg-mainpink dark:bg-mainyellow px-[40px] py-[10px]
                         ${
                             menuOpen 
                                 ? "opacity-100 translate-y-0"
